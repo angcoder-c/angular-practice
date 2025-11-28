3f3bcbdfe70525c9ec0ec4f2a0ab3568ee8a6c8e
angc <angcoder.c@gmail.com>
2025-11-28 11:51:25 -0600

    unsibscribe for memory leaks

--- rxjs-practice/src/app/components/basic-operators/basic-operators.component.ts
+++ rxjs-practice/src/app/components/basic-operators/basic-operators.component.ts
@@ -1,5 +1,5 @@
 import { Component, numberAttribute } from '@angular/core';
-import { delay, delayWhen, filter, map, merge, of, single, tap, timer } from 'rxjs';
+import { delay, delayWhen, filter, map, merge, of, single, Subscription, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'app-basic-operators',
@@ -12,36 +12,51 @@ export class BasicOperatorsComponent {
   private data = of(1,2,3,4)
   private other = of(5,6,7)
 
+  subscribes: Subscription[] = []
+
   ngOnInit() {
-    this.data
-    .pipe(
-      //delay sirve para ponerle una espera a las emisiones
-      delay(1000),
-      // delayWhen(()=>timer(1000)),
-      // mapear los valores emitidos por un observable
-      map((num: number) => num+1),
-      // espejo del operador anterior, útil para debugear
-      tap((num: number) => console.log('EMISIONES CON DELAY')),
-      tap((num: number) => console.log(num))
+    this.subscribes.push(
+      this.data
+      .pipe(
+        //delay sirve para ponerle una espera a las emisiones
+        delay(1000),
+        // delayWhen(()=>timer(1000)),
+        // mapear los valores emitidos por un observable
+        map((num: number) => num+1),
+        // espejo del operador anterior, útil para debugear
+        tap((num: number) => console.log('EMISIONES CON DELAY')),
+        tap((num: number) => console.log(num))
+      )
+      .subscribe()
     )
-    .subscribe()
 
-    //unir de forma asincrona dos observables
-    merge(this.data, this.other).pipe(
-      tap((res: number) => console.log('MERGE')),
-      tap((res: number) => console.log(res))
-    ).subscribe()
+    this.subscribes.push(
+      //unir de forma asincrona dos observables
+      merge(this.data, this.other).pipe(
+        tap((res: number) => console.log('MERGE')),
+        tap((res: number) => console.log(res))
+      ).subscribe()
+    )
 
-    this.data.pipe(
-      filter((num: number) => num % 2 === 0),
-      tap((res: number) => console.log('FILTER')),
-      tap((res: number) => console.log(res))
-    ).subscribe()
+    this.subscribes.push(
+      this.data.pipe(
+        filter((num: number) => num % 2 === 0),
+        tap((res: number) => console.log('FILTER')),
+        tap((res: number) => console.log(res))
+      ).subscribe()
+    )
 
-    this.data.pipe(
-      single((num: number) => num === 3),
-      tap((res: number) => console.log('SINGLE')),
-      tap((res: number) => console.log(res))
-    ).subscribe()
+    this.subscribes.push(
+      this.data.pipe(
+        single((num: number) => num === 3),
+        tap((res: number) => console.log('SINGLE')),
+        tap((res: number) => console.log(res))
+      ).subscribe()
+    )
+  }
+
+  ngOnDestroy () {
+    console.log('clean')
+    this.subscribes.forEach(sub => sub.unsubscribe())
   }
 }
